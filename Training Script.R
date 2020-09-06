6544fab8fa62758717f6d92868590de8d62c9c31
NehemiahDaGoat <50632115+NehemiahDaGoat@users.noreply.github.com>
2020-09-06 15:30:04 -0400

    Nehemiah S. 09_06_20 This is hard.

--- Training Script.R
+++ Training Script.R
@@ -8,16 +8,35 @@ library(pracma)
 
 #I opened the Here library
 library(here)
+library(plotly)
+here()
 
 ##Opening a CSV file the Long Way
 LongWay<-read.csv("C:/Users/14434/Desktop/VectorRideshareDataAnalysis/Coding Ready Data.csv")
 
 ##Here, I uploaded a CSV through the Read.csv function
 TestFile<-read.csv(here("Coding Ready Data.csv"))
-X<-TestFile$WaveNum
+Wagging<-TestFile[c(1000:25000),]
 
-Date<-Testfile$DateTime
-class(Date)
+
+Wavey<-Wagging$WaveNum
+Transmit<-Wagging$PCT_Transmit
 
 ##Let's Make a Plot
-TestPlot<-ggplot(data = Testfile, aes())
+FTIR<-read.csv(here("Coding Ready Data.csv"))
+Wavenumber<-FTIR$WaveNum
+Transmit<-FTIR$PCT_Transmit
+
+
+##Format of GGPLOT function [ggplot(data,aes(x,y))+geom_point()
+TestPlot<-ggplot(FTIR,aes(Wavenumber,Transmit))+
+  geom_point()
+TestPlot     
+
+##Make a new data table, that is a subset of the larger data table.
+Wagging<- FTIR[c(11000:30000),]
+Wavenumber2<-Wagging$WaveNum
+Transmit2<-Wagging$PCT_Transmit
+TestPlot2<-ggplot(Wagging,aes(Wavenumber2,Transmit2))+
+  geom_point()
+TestPlot2
